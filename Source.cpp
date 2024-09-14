be9faf67e6047dad424ade5250a24d31d1e009b4
DeDe136 <hehehe8011@gmail.com>
2024-09-14 14:29:22 +0700

    Add Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+void SangNTtrendoan(int l, int r)
+{
+	int a = r - l + 1;
+	bool *nt;
+	nt = new bool(a);
+	for (int i = 0; i < r - l; i++)
+	{
+		nt[i] = true;
+		cout << nt[i] << " ";
+	}
+}
+int main()
+{
+	int l, r;
+	cin >> l >> r;
+	SangNTtrendoan(l, r);
+	return 1;
+}
